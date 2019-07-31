4f478ad3956b294d12486e1a1795830671dceb64
tinggirl00 <18210240180@fudan.edu.cn>
2019-07-31 12:54:50 +0800

    use generatorConfig.xml to simple databse

--- src/main/java/com/example/demo1/controller/PublishController.java
+++ src/main/java/com/example/demo1/controller/PublishController.java
@@ -1,12 +1,15 @@
 package com.example.demo1.controller;
 
+import com.example.demo1.dto.QuestionDTO;
 import com.example.demo1.mapper.QuestionMapper;
 import com.example.demo1.model.Question;
 import com.example.demo1.model.User;
+import com.example.demo1.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -15,11 +18,23 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class PublishController {
 
+
     @Autowired
-    private QuestionMapper questionMapper;
+    private QuestionService questionService;
+
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        model.addAttribute("id",question.getId());
+        return "publish";
+    }
 
     @GetMapping("/publish")
-    public String publish(){
+    public String publish() {
         return "publish";
     }
 
@@ -28,6 +43,7 @@ public class PublishController {
             @RequestParam(value = "title", required = false) String title,
             @RequestParam(value = "description", required = false) String description,
             @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false)Integer id,
             HttpServletRequest request,
             Model model) {
         model.addAttribute("title", title);
@@ -48,8 +64,7 @@ public class PublishController {
         }
 
 
-
-        User user = (User)request.getSession().getAttribute("user");
+        User user = (User) request.getSession().getAttribute("user");
 
         if (user == null) {
             model.addAttribute("error", "用户未登录");
@@ -60,9 +75,9 @@ public class PublishController {
         question.setDescription(description);
         question.setTag(tag);
         question.setCreator(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
-        questionMapper.create(question);
+
+        question.setId(id);
+        questionService.createOrUpdate(question);
 
 
         return "redirect:/";
--- src/main/java/com/example/demo1/service/QuestionService.java
+++ src/main/java/com/example/demo1/service/QuestionService.java
@@ -5,7 +5,10 @@ import com.example.demo1.dto.QuestionDTO;
 import com.example.demo1.mapper.QuestionMapper;
 import com.example.demo1.mapper.UserMapper;
 import com.example.demo1.model.Question;
+import com.example.demo1.model.QuestionExample;
 import com.example.demo1.model.User;
+import com.example.demo1.model.UserExample;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,13 +31,12 @@ public class QuestionService {
 
         Integer totalPage;
         //得到总数
-        Integer totalCount = questionMapper.count();
+        Integer totalCount = (int) questionMapper.countByExample(new QuestionExample());
 
-        if(totalCount % size ==0){
+        if (totalCount % size == 0) {
             totalPage = totalCount / size;
-        }
-        else {
-            totalPage = totalCount / size +1;
+        } else {
+            totalPage = totalCount / size + 1;
         }
 
         paginationDTO.setPagination(totalPage, page);
@@ -51,18 +53,23 @@ public class QuestionService {
         Integer offset = size * (page - 1);
 
         //
-        List<Question> questions = questionMapper.list(offset, size);
+//        List<Question> questions = questionMapper.list(offset, size);
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(new QuestionExample(), new RowBounds(offset, size));
+
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for (Question question : questions) {
-            User user = userMapper.findById(question.getCreator());
+            UserExample userExample = new UserExample();
+            userExample.createCriteria().andIdEqualTo(question.getCreator());
+            List<User> user = userMapper.selectByExample(userExample);
+
 
             QuestionDTO questionDTO = new QuestionDTO();
 
             //spring内置了copy方法
             BeanUtils.copyProperties(question, questionDTO);
 
-            questionDTO.setUser(user);
+            questionDTO.setUser(user.get(0));
             questionDTOList.add(questionDTO);
         }
         paginationDTO.setQuestions(questionDTOList);
@@ -76,12 +83,16 @@ public class QuestionService {
         Integer totalPage;
         PaginationDTO paginationDTO = new PaginationDTO();
         //得到总数
-        Integer totalCount = questionMapper.countUserById(userId);
-        if(totalCount % size ==0){
+
+        //Integer totalCount = questionMapper.countUserById(userId);
+        QuestionExample questionExample = new QuestionExample();
+        questionExample.createCriteria().andCreatorEqualTo(userId);
+        Integer totalCount = (int) questionMapper.countByExample(questionExample);
+
+        if (totalCount % size == 0) {
             totalPage = totalCount / size;
-        }
-        else {
-            totalPage = totalCount / size +1;
+        } else {
+            totalPage = totalCount / size + 1;
         }
 
         paginationDTO.setPagination(totalPage, page);
@@ -98,18 +109,23 @@ public class QuestionService {
         Integer offset = size * (page - 1);
 
         //
-        List<Question> questions = questionMapper.userIdList(userId,offset, size);
+        QuestionExample example = new QuestionExample();
+        example.createCriteria().andCreatorEqualTo(userId);
+//        List<Question> questions = questionMapper.userIdList(userId,offset, size);
+        List<Question> questions = questionMapper.selectByExampleWithRowbounds(example, new RowBounds(offset, size));
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
         for (Question question : questions) {
-            User user = userMapper.findById(question.getCreator());
+            UserExample userExample = new UserExample();
+            userExample.createCriteria().andIdEqualTo(question.getCreator());
+            List<User> user = userMapper.selectByExample(userExample);
 
             QuestionDTO questionDTO = new QuestionDTO();
 
             //spring内置了copy方法
             BeanUtils.copyProperties(question, questionDTO);
 
-            questionDTO.setUser(user);
+            questionDTO.setUser(user.get(0));
             questionDTOList.add(questionDTO);
         }
         paginationDTO.setQuestions(questionDTOList);
@@ -118,15 +134,62 @@ public class QuestionService {
     }
 
     public QuestionDTO getById(Integer id) {
-        Question question = questionMapper.getById(id);
-        QuestionDTO questionDTO = new QuestionDTO();
-        BeanUtils.copyProperties(question,questionDTO);
 
         /**
          * 注意：此处的id是问题的id，而不是用户的；
          * */
-        User user = userMapper.findById(question.getCreator());
+        QuestionExample example = new QuestionExample();
+        example.createCriteria().andIdEqualTo(id);
+
+        /**
+         * 之所以description字段内容显示不出来，是因为selectByExample(）方法只能检测小字段的类型
+         * 大字段类型，如text，需使用selectByExampleWithBLOBs()方法
+         * */
+        Question question = questionMapper.selectByExampleWithBLOBs(example).get(0);
+        //  Question question = questionMapper.selectByExample(example).get(0);
+
+        QuestionDTO questionDTO = new QuestionDTO();
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andIdEqualTo(question.getCreator());
+        User user = userMapper.selectByExample(userExample).get(0);
         questionDTO.setUser(user);
+        BeanUtils.copyProperties(question, questionDTO);
+
+
         return questionDTO;
+    }
+
+    /**
+     * 发布问题：（1）第一次发布
+     *         （2）更新已发布的信息
+     * */
+    public void createOrUpdate(Question question) {
+
+        if (question.getId() == null) {
+            /** 第一次创建
+             * */
+            question.setGmtCreate(System.currentTimeMillis());
+            question.setGmtModified(question.getGmtCreate());
+            /**
+             * 访问数、浏览数、点赞数初始时均为0
+             * */
+            question.setViewCount(0);
+            question.setLikeCount(0);
+            question.setCommentCount(0);
+            questionMapper.insert(question);
+        } else {
+            /** 更新
+             * */
+            Question updateQuestion = new Question();
+            updateQuestion.setGmtModified(System.currentTimeMillis());
+            updateQuestion.setTag(question.getTag());
+            updateQuestion.setTitle(question.getTitle());
+            updateQuestion.setDescription(question.getDescription());
+            QuestionExample questionExample = new QuestionExample();
+            //  question.setGmtModified(question.getGmtCreate());
+            questionExample.createCriteria().andIdEqualTo(question.getId());
+            questionMapper.updateByExampleSelective(updateQuestion, questionExample);
+
+        }
     }
 }
